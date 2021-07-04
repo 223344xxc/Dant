9e8c92fa867db24d8016e6622aa2c2c9a202f67d
223344xxc <74497675+223344xxc@users.noreply.github.com>
2021-07-04 14:40:36 +0900

    dant

---
+++ Assets/Script/PlayerCtrl.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public Vector3 MoveDir = Vector3.zero;
+    Vector3 PlayerStartingScale;
+    Vector3 TempVector;
+
+
+    public float MoveSpeed = 10;
+    float TempMoveSpeed = 0;
+
+    public Animator animator;
+
+    Rigidbody PlayerRigid;
+
+    void Start()
+    {
+        InitPlayer();
+    }
+
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void InitPlayer()
+    {
+        PlayerStartingScale = transform.localScale;
+        PlayerRigid = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+
+    }
+
+
+    void PlayerMove()
+    {
+        MoveDir.x = Input.GetAxisRaw("Horizontal");
+        if (Input.GetMouseButtonDown(1))
+        {
+            Attack();
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Dash();
+        }
+
+        if (MoveDir.x > 0)
+        {
+            TempVector = PlayerStartingScale;
+            TempVector.x = -PlayerStartingScale.x;
+            transform.localScale = TempVector;
+        }
+        else if (MoveDir.x < 0)
+        {
+            TempVector = PlayerStartingScale;
+            transform.localScale = TempVector;
+        }
+
+        TempVector.x = MoveDir.x * MoveSpeed;
+        TempVector.y = PlayerRigid.velocity.y;
+        TempVector.z = PlayerRigid.velocity.z;
+
+        PlayerRigid.velocity = TempVector;
+        animator.SetFloat("MoveVel", Mathf.Abs(PlayerRigid.velocity.x));
+        
+    }
+
+    void ResetMoveSpeed()
+    {
+        MoveSpeed = TempMoveSpeed;
+    }
+
+    void ChangeAttackStateToFalse()
+    {
+        animator.SetBool("IsAttack", false);
+    }
+
+    void ChangeAttackStateToTrue()
+    {
+
+        animator.SetBool("IsAttack", true);
+    }
+
+    void ChangeNextAttackToFalse()
+    {
+        animator.SetBool("NextAttack", false);
+    }
+
+    void ChangeNextAttackToTrue()
+    {
+        animator.SetBool("NextAttack", true);
+    }
+
+
+    void Attack()
+    {
+        if(animator.GetBool("IsAttack") == true)
+        {
+            animator.SetBool("NextAttack", true);
+        }
+        animator.SetBool("IsAttack", true);
+    }
+
+    void Jump()
+    {
+        PlayerRigid.AddForce(0, 300, 0);
+    }
+
+    void Dash()
+    {
+        animator.SetTrigger("IsDash");
+        TempMoveSpeed = MoveSpeed;
+        MoveSpeed *= 3;
+    }
+}
---
+++ Assets/Script/TestExplod.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestExplod : MonoBehaviour
+{
+    public GameObject ExplodPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(ExplodPrefab);
+            transform.localScale = Vector3.zero;
+            Invoke("Reset_I", 1);
+        }
+    }
+
+    void Reset_I()
+    {
+        transform.localScale = Vector3.one;
+    }
+
+}
