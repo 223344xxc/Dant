f17e2086e287cdff654cf5f04e51b48807bb2f90
223344xxc <74497675+223344xxc@users.noreply.github.com>
2021-07-27 22:48:29 +0900

    UPDATE - 씬 관리 코드 추가
    MainCtrl, SceneCtrl 코드 추가

---
+++ Assets/Script/Ctrl/MainCtrl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCtrl : MonoBehaviour
+{
+    private string sceneName;
+    public string SceneName
+    {
+        get => sceneName;
+        set => sceneName = value;
+    }
+
+
+    void Start()
+    {
+        //씬 넘기기 테스트
+        sceneName = "StageSelectScene";
+        DontDestroyOnLoad(this);
+    }
+
+    void Update()
+    {
+        //임시 씬 전환.. 나중에 구조를 바꿀 예정
+        if (Input.GetMouseButton(0) && sceneName != "None") {
+            SceneCtrl.LoadScene(SceneName);
+            
+            if(sceneName == "StageSelectScene")
+            {
+                sceneName = "InGameScene";
+            }
+            else
+            {
+                sceneName = "None";
+            }
+        }
+
+    }
+}
