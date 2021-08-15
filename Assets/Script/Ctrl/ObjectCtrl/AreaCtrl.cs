003540692ba83375310a8b9f811abc3e509ef019
223344xxc <74497675+223344xxc@users.noreply.github.com>
2021-08-15 19:40:22 +0900

    UPDATE - 플래이어 충돌 판정 업데이트

---
+++ Assets/Script/Ctrl/ObjectCtrl/AreaCtrl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaCtrl : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("MONSTER"))
+        {
+            PlayerCtrl.AddAttackToObject(collision.gameObject.GetComponent<EnemyCtrl>().Damaged);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("MONSTER"))
+        {
+            PlayerCtrl.RemoveAttackToObject(collision.gameObject.GetComponent<EnemyCtrl>().Damaged);
+        }
+    }
+}
