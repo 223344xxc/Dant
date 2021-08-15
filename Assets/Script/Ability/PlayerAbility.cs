14eb735cc0a28e8963a14a25b7c16a8942992efb
223344xxc <74497675+223344xxc@users.noreply.github.com>
2021-08-15 19:34:00 +0900

    UPDATE - 플레이어 능력치 업데이트

--- Assets/Script/Ability/PlayerAbility.cs
+++ Assets/Script/Ability/PlayerAbility.cs
@@ -6,8 +6,7 @@ public class PlayerAbility : Ability
 {
     [Header("플래이어 능력치")]
     [Tooltip("점프력")]
-    [SerializeField]
-    private float jumpPower;
+    [SerializeField] private float jumpPower;
     public float JumpPower
     {
         get => jumpPower;
@@ -15,12 +14,22 @@ public class PlayerAbility : Ability
     }
 
     [Tooltip("대쉬 속도")]
-    [SerializeField]
-    private float dashSpeed;
+    [SerializeField] private float dashSpeed;
 
     public float DashSpeed
     {
         get => dashSpeed;
         set => dashSpeed = value;
     }
+
+    [SerializeField] private float invincibilityTime;
+    public float InvincibilityTime
+    {
+        get => invincibilityTime;
+        set => invincibilityTime = value;
+
+    }
+
+    protected int HideCount_Start = 3;
+    protected int HideCount_End = 5;
 }
