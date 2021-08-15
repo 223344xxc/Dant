9c95077a0fc0e8c5ca15b6b73661ec16750fff2c
223344xxc <74497675+223344xxc@users.noreply.github.com>
2021-08-15 19:41:28 +0900

    UPDATE - 디버그 코드 업데이트

---
+++ Assets/Script/Report/Report.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Report
+{
+    public static void Log(object Object)
+    {
+#if UNITY_EDITOR
+        Debug.Log(Object.ToString());
+#endif
+    }
+
+    public static void LogError(object Object)
+    {
+#if UNITY_EDITOR
+        Debug.LogError(Object.ToString());
+#endif
+    }
+}
