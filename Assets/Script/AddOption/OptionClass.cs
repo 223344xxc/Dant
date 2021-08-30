042557a6154516293302f9cda7db5d55de06f786
223344xxc <74497675+223344xxc@users.noreply.github.com>
2021-08-31 00:52:10 +0900

    UPDATE - 추가 기능이 있는 Option 클래스 추가 및 AddOption 수정

---
+++ Assets/Script/AddOption/OptionClass.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class OptionClass
+{
+    public static void CopyArr<T>(ref T[] c1, T[] c2)
+    {
+        c1 = new T[c2.Length];
+        c1 = c2;
+    }
+}
--- Assets/Script/AddOption/Report.cs
+++ Assets/Script/AddOption/Report.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Report
+public static class Report
 {
-    public static void Log(object Object)
+    public static void Log(this object Object)
     {
 #if UNITY_EDITOR
         Debug.Log(Object.ToString());
 #endif
     }
 
-    public static void LogError(object Object)
+    public static void LogError(this object Object)
     {
 #if UNITY_EDITOR
         Debug.LogError(Object.ToString());
