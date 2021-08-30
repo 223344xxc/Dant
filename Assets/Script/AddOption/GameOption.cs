a75af921ce5ad09a1b4a15f287ad1a08a1998a19
223344xxc <74497675+223344xxc@users.noreply.github.com>
2021-08-31 00:54:31 +0900

    UPDATE - 인게임 흐름을 관리하는 GameMgr 과 GameOption 추가

---
+++ Assets/Script/AddOption/GameOption.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum GameState
+{
+    None,
+    Play,
+    Pause,
+    Over,
+}
+
+public static class GameOption
+{
+    public static Func<GameState> getGameState;
+    public static Action<GameState> setGameState;
+
+    private static Action startGame;
+    private static Action endGame;
+
+    public static GameState? GetGameState()
+    {
+        return getGameState?.Invoke();
+    }
+
+    public static void SetGameState(GameState state)
+    {
+        setGameState?.Invoke(state);
+    }
+
+    public static bool IsGameState(GameState state)
+    {
+        return state == getGameState?.Invoke();
+    }
+
+    public static void BindStartGameFunction(Action startgame) { startGame = startgame; }
+    public static void BindEndGameFunction(Action endgame) { endGame = endgame; }
+    public static void StartGame()
+    {
+        startGame?.Invoke();
+    }
+
+    public static void EndGame()
+    {
+        endGame?.Invoke();
+    }
+    
+}
---
+++ Assets/Script/Manager/GameMgr.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMgr : MonoBehaviour
+{
+    private static GameState gameState = GameState.None;
+
+    private PlayerCtrl player;
+
+    public static void SetGameState(GameState gameState)
+    {
+        Debug.Log(gameState);
+        GameMgr.gameState = gameState;
+    }
+    public static GameState GetGameState()
+    {
+        return GameMgr.gameState;
+    }
+
+    public void Awake()
+    {
+        Time.timeScale = 0;
+        InitGame();
+    }
+
+    public void InitGame()
+    {
+        GameOption.getGameState = () => { return GetGameState(); };
+        GameOption.setGameState = (GameState state) => { SetGameState(state); };
+        GameOption.BindStartGameFunction(StartGame);
+        GameOption.BindEndGameFunction(EndGame);
+        player = GameObject.FindWithTag("Player").GetComponent<PlayerCtrl>();
+        
+    }
+
+    public static void StartGame()
+    {
+        Time.timeScale = 1;
+        SetGameState(GameState.Play);
+    }
+
+    public static void EndGame()
+    {
+        SetGameState(GameState.Over);
+    }
+}
