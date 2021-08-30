2eac315c4c74df5f2495a5cfe44ee1dbea2548d7
223344xxc <74497675+223344xxc@users.noreply.github.com>
2021-08-31 00:58:59 +0900

    UPDATE - 스테이지 선택화면 버튼 추가

---
+++ Assets/Script/UI/ButtonCtrl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonCtrl : MonoBehaviour
+{
+    private MainCtrl main;
+    private void Start()
+    {
+        main = GameObject.FindGameObjectWithTag("MAIN").GetComponent<MainCtrl>();        
+    }
+
+    public void SelectStage(string StageName)
+    {
+        main.LoadScene(StageName);
+    }
+}
