ed5706184c10d80847c337cbb941422bba41f674
223344xxc <74497675+223344xxc@users.noreply.github.com>
2021-09-05 18:48:37 +0900

    UPDATE - 게임 종료 후 결과 창 추가, UI 보여주는 함수 수정

--- Assets/Script/UI/UICtrl.cs
+++ Assets/Script/UI/UICtrl.cs
@@ -6,10 +6,23 @@ using UnityEngine.UI;
 
 public class UICtrl : MonoBehaviour
 {
-    [SerializeField] private GameObject PauseUi;
-    [SerializeField] private GameObject InGameUi;
-    [SerializeField] private Image FadeImage;
-    
+    enum UiState
+    {
+        None,
+        Pause,
+        InGame,
+        EndGame
+    }
+
+    [SerializeField] private Image fadeImage;
+
+    [SerializeField] private Sprite WinBackGround;
+    [SerializeField] private Sprite LoseBackGround;
+
+    private Dictionary<UiState, GameObject> UiList;
+    private Text PlayerHpText;
+    private Image EndGameBackGround;
+
     [SerializeField] private Text minText;
     [SerializeField] private Text secText;
 
@@ -21,7 +34,26 @@ public class UICtrl : MonoBehaviour
 
     public void Awake()
     {
+        InitUi();
+        ShowUi(UiState.InGame);
         PlayerCtrl.AddGameOverFun(FadeIn);
+    }
+
+    private void InitUi()
+    {
+        UiList = new Dictionary<UiState, GameObject>();
+        AddUiDictionary(UiState.InGame, "InGame");
+        AddUiDictionary(UiState.Pause, "Pause");
+        AddUiDictionary(UiState.EndGame, "EndGame");
+        PlayerHpText = UiList[UiState.EndGame].transform.Find("EndGamePanel").transform.Find("Hart").transform.Find("HartCount").GetComponent<Text>();
+        EndGameBackGround = UiList[UiState.EndGame].transform.Find("EndGamePanel").transform.Find("BackGround").GetComponent<Image>();
+
+    }
+
+    private void AddUiDictionary(UiState uiState, string uiName)
+    {
+
+        UiList.Add(uiState, GameObject.Find(uiName));
     }
 
     public void PauseGame()
@@ -29,18 +61,41 @@ public class UICtrl : MonoBehaviour
         
         if (Time.timeScale != 0)
         {
-            PauseUi.SetActive(true);
-            InGameUi.SetActive(false);
+            ShowUi(UiState.Pause);
             GameOption.SetGameState(GameState.Pause);
             Time.timeScale = 0;
         }
         else
         {
-            PauseUi.SetActive(false);
-            InGameUi.SetActive(true);
+            ShowUi(UiState.InGame);
             GameOption.SetGameState(GameState.Play);
             Time.timeScale = 1;
         }
+    }
+
+    private void ShowUi(UiState uiState)
+    {
+        for (UiState i = UiState.Pause; (int)i < (int)UiState.EndGame + 1; i++)
+        {
+            UiList[i].SetActive(false);
+        }
+
+        switch (uiState)
+        {
+            case UiState.EndGame:
+                EndGameBackGround.sprite = PlayerCtrl.Instance.Hp > 0 ? WinBackGround : LoseBackGround;
+                PlayerHpText.text = "X " + PlayerCtrl.Instance.Hp;
+                break;
+            default:
+                break;
+        }
+
+        UiList[uiState].SetActive(true);
+    }
+
+    private void ShowEndGameUi()
+    {
+        ShowUi(UiState.EndGame);
     }
 
     public void Start()
@@ -56,12 +111,12 @@ public class UICtrl : MonoBehaviour
     }
     public void FadeOut()
     {
-        FadeImage.enabled = true;
+        fadeImage.enabled = true;
         StartCoroutine(FadeScreen(true));
     }
     public void FadeIn()
     {
-        FadeImage.enabled = true;
+        fadeImage.enabled = true;
         StartCoroutine(FadeScreen(false));
     }
 
@@ -71,16 +126,17 @@ public class UICtrl : MonoBehaviour
         {
             fadeColor.a = fadeState ? 0f : 0.5f;
 
-            FadeImage.color = Color.Lerp(this.FadeImage.color, fadeColor, this.fadeSpeed);
-            if (fadeState && FadeImage.color.a <= 0.1f)
+            fadeImage.color = Color.Lerp(this.fadeImage.color, fadeColor, this.fadeSpeed);
+            if (fadeState && fadeImage.color.a <= 0.1f)
             {
-                FadeImage.enabled = false;
+                fadeImage.enabled = false;
                 GameOption.StartGame();
                 break;
             }
-            else if (!fadeState && FadeImage.color.a >= 0.45f)
+            else if (!fadeState && fadeImage.color.a >= 0.45f)
             {
                 GameOption.EndGame();
+                ShowEndGameUi();
                 break;
             }
 
