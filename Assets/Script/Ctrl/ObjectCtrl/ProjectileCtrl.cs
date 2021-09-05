77fb231f46020eb3bead10181656844b4dec70c9
223344xxc <74497675+223344xxc@users.noreply.github.com>
2021-09-05 18:39:17 +0900

    UPDATE - 플레이어의 투사체 이동 및 충돌 추가

---
+++ Assets/Script/Ctrl/ObjectCtrl/ProjectileCtrl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileCtrl : MonoBehaviour
+{
+    [SerializeField] private float Speed;
+
+    private void Awake()
+    {
+        Destroy(gameObject, 10);
+    }
+
+    private void Update()
+    {
+        transform.position += transform.localScale.x > 0 ? 
+            -transform.right * Time.deltaTime * Speed : 
+             transform.right * Time.deltaTime * Speed; 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("MONSTER"))
+        {
+            EnemyCtrl ec = collision.gameObject.GetComponent<EnemyCtrl>();
+
+            ec.Damaged_Enemy(PlayerCtrl.Instance.AttackDamage);
+        }
+
+    }
+}
