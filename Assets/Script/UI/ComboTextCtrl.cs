e3efa8d0563e6717a2ac00883d5a9e19b5446405
223344xxc <74497675+223344xxc@users.noreply.github.com>
2021-09-05 18:41:08 +0900

    UPDATE - 콤보 애니메이션 추가

--- Assets/Script/UI/ComboTextCtrl.cs
+++ Assets/Script/UI/ComboTextCtrl.cs
@@ -5,15 +5,30 @@ using UnityEngine.UI;
 
 public class ComboTextCtrl : MonoBehaviour
 {
-    public Text ComboText;
-    public Text BackComboText;
+    [SerializeField] private Text ComboText;
+    [SerializeField] private Text BackComboText;
+    [SerializeField] private Animator anim;
 
+    private bool canPlay = true;
 
     private void Start()
     {
         PlayerCtrl.AddUpdateUIFun((PlayerCtrl player) => {
+            anim.Play("ComboAnim");
             ComboText.text = player.Combo + " Combo";
             BackComboText.text = player.Combo + " Combo";
         });
     }
+
+    #region 이벤트
+    public void StartPlay()
+    {
+        canPlay = false;
+    }
+
+    public void EndPlay()
+    {
+        canPlay = true;
+    }
+    #endregion
 }
