574eadb35c4374f9457d68002e19569b643a7640
223344xxc <74497675+223344xxc@users.noreply.github.com>
2021-09-09 23:22:46 +0900

    UPDATE - EnemyCtrl 을 상속받는 폭탄벌래 스크립트 추가

---
+++ Assets/Script/Ctrl/Character/BomBugCtrl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BomBugCtrl : EnemyCtrl
+{
+    [Header("폭탄벌래 폭발 범위")]
+    [SerializeField] private float explodRadius;
+    [SerializeField] private Vector3 explodOffset;
+
+    protected override void Update()
+    {
+        base.Update();
+
+        ExplodChack();
+    }
+
+    private void ExplodChack()
+    {
+        Vector3.Distance(explodOffset + transform.position, PlayerCtrl.Instance.transform.position).Log();
+        if (Vector3.Distance(explodOffset + transform.position, PlayerCtrl.Instance.transform.position) < explodRadius)
+        {
+            "explod".LogError();
+            MoveSpeed = 0;
+            animator.Play("Explosion");
+        }
+    }
+
+#if UNITY_EDITOR
+    protected override void OnDrawGizmosSelected()
+    {
+        base.OnDrawGizmosSelected();
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(explodOffset + transform.position, explodRadius);
+    }
+#endif
+}
